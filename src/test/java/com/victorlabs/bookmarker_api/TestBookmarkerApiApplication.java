c78e823e92f44b45ad238b49874c453d675ff0fb
victorfneves99 <victorfneves99@gmail.com>
2024-08-10 20:05:33 -0300

    primeiro commit

---
+++ src/test/java/com/victorlabs/bookmarker_api/TestBookmarkerApiApplication.java
@@ -0,0 +1,11 @@
+package com.victorlabs.bookmarker_api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBookmarkerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BookmarkerApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
